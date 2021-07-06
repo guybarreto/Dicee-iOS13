1a1fdec37abf5b4f406eb610dbdb44728c621ee7
Guy Barreto <85413584+guybarreto@users.noreply.github.com>
2021-07-06 12:22:32 -0300

    Dices rolling randomly

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // IB = Interface Builder
+    // IBOutlet let me allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // We can choose a diceArray's random element in different ways:
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]  // It'll randomly choose a integer number between 0 and 5, that corresponds to a deceArray's element.
+        diceImageView2.image = diceArray.randomElement()  // It'll see how many elements diceArray have and select one randomly. It's a cleaner way.
+     
+    
     }
-
-
 }
-
