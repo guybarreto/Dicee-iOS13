6c19c9098bf7dea90a84c8ec098a0ee9d27b5e44
Guy Barreto <85413584+guybarreto@users.noreply.github.com>
2021-07-27 12:29:16 -0300

    Finished

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
         // We can choose a diceArray's random element in different ways:
-        diceImageView1.image = diceArray[Int.random(in: 0...5)]  // It'll randomly choose a integer number between 0 and 5, that corresponds to a deceArray's element.
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]  // It'll randomly choose a integer number between 0 and 5, that corresponds to a diceArray's element.
         diceImageView2.image = diceArray.randomElement()  // It'll see how many elements diceArray have and select one randomly. It's a cleaner way.
      
     
